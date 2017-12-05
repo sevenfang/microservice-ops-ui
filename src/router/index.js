5014cb42763a1adcecffaddc72ea8469bd7dfd6a
DESKTOP-ISUL2SA\daniell <daniell_hu@163.com>
2017-12-05 11:12:03 +0800

    新增第三方认证页面和忽略配置页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-const _import = require('./_import_' + process.env.NODE_ENV);
-// const __import = require('_import_' + process.env.NODE_ENV);
+import Vue from 'vue'
+import Router from 'vue-router'
+const _import = require('./_import_' + process.env.NODE_ENV)
+// const __import = require('_import_' + process.env.NODE_ENV)
 // in development env not use Lazy Loading,because Lazy Loading large page will cause webpack hot update too slow.so only in production use Lazy Loading
 
-Vue.use(Router);
+Vue.use(Router)
 
 /* layout */
-import Layout from '../views/layout/Layout';
-
+import Layout from '../views/layout/Layout'
 
 /**
  * icon : the icon show in the sidebar
@@ -85,6 +84,31 @@ export const constantRouterMap = [{
   }]
 },
 {
+  path: '/gate/gateAuthProvider',
+  component: Layout,
+  redirect: '/gate/authprovider',
+  icon: 'form',
+  noDropdown: true,
+  children: [{
+    path: '/',
+    component: _import('gate/authprovider'),
+    name: '动态过滤'
+  }]
+},
+{
+  path: '/gate/gateIgnoreUri',
+  component: Layout,
+  redirect: '/gate/gateIgnoreUri',
+  icon: 'form',
+  noDropdown: true,
+  children: [{
+    path: '/',
+    component: _import('gate/gateIgnoreUri'),
+    name: '动态过滤'
+  }]
+},
+
+{
   path: '/message',
   component: Layout,
   redirect: '/message/index',
@@ -104,7 +128,7 @@ export default new Router({
     y: 0
   }),
   routes: constantRouterMap
-});
+})
 
 export const asyncRouterMap = [{
   path: '/baseManager',
@@ -143,7 +167,7 @@ export const asyncRouterMap = [{
     name: '操作日志管理',
     authority: 'gateLogManager'
   }]
-}];
+}]
 
 export const asyncRouterMap2 = [{
   path: '/gateManager',
@@ -166,4 +190,4 @@ export const asyncRouterMap2 = [{
   //   authority: 'gateLogManager'
   // }
   ]
-}];
+}]
