4ead889bd712be1c4028d9ee9ae43b7488095f02
DESKTOP-ISUL2SA\daniell <daniell_hu@163.com>
2018-01-09 18:03:30 +0800

    url

--- src/api/task/tasklog.js
+++ src/api/task/tasklog.js
@@ -1,7 +1,7 @@
 import fetch from 'utils/fetch';
 export function page(query) {
     return fetch({
-        url: '/api/task/tasklogs.json',
+        url: '/api/task/taskManager/tasklogs.json',
         method: 'get',
         params: query
     });
